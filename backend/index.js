17e99c08f020e40f76cd570b30a046d1e4543ad4
Malaika Khan <malaika.r.khan@gmail.com>
2026-09-12 02:07:22 +0100

    Initial Backend commit

---
+++ backend/index.js
@@ -0,0 +1,22 @@
+import cors from "cors"
+import dotenv from "dotenv"
+import express from "express"
+import mongoose from "mongoose"
+import morgan from "morgan"
+
+dotenv.config()
+
+const app = express()
+app.use(express.json())
+
+const PORT = process.env.PORT || 5000
+
+app.get("/", async(req, res)=> {
+    res.status(200).json({
+        message:"Welcome to Project Management API"
+    })
+})
+
+app.listen(PORT, ()=> {
+    console.log(`Server running on port ${PORT}`)
+})
--- frontend/app/lib/schema.ts
+++ frontend/app/lib/schema.ts
@@ -2,14 +2,14 @@ import { z } from "zod";
 
 export const signinSchema = z.object({
     email: z.string().email("Invalid email address"),
-    password: z.string().min(8, "Password must be at least 8 characters long").max(50, "Password must not exceed 50 characters"),
+    password: z.string().min(8, "Password must be at least 8 characters").max(50, "Password must not exceed 50 characters"),
 })
 
 export const signupSchema = z.object({
     name: z.string().min(3, "Name must be at least 3 characters").max(50, "Name must not exceed 50 characters"),
     email: z.string().email("Invalid email address"),
-    password: z.string().min(8, "Password must be at least 8 characters long").max(50, "Password must not exceed 50 characters"),
-    confirmPassword: z.string().min(8, "Password must be 8 characters"),
+    password: z.string().min(8, "Password must be at least 8 characters").max(50, "Password must not exceed 50 characters"),
+    confirmPassword: z.string().min(8, "Password must at least be 8 characters"),
 }).refine((data) => data.password === data.confirmPassword, {
     path: ["confirmPassword"],
     message: "Passwords do not match",
