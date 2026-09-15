85b69dcd411257d677867666895d86a1cf801909
Malaika Khan <malaika.r.khan@gmail.com>
2026-09-15 23:46:24 +0100

    Backend Setup

--- backend/index.js
+++ backend/index.js
@@ -7,6 +7,20 @@ import morgan from "morgan"
 dotenv.config()
 
 const app = express()
+
+app.use(cors({
+    origin: process.env.FRONTEND_URL,
+    methods: ["GET", "POST", "DELETE", "PUT"],
+    allowedHeaders: ['Content-Type', 'Authorization']
+}))
+
+app.use(morgan("dev"))
+
+//db connection!!
+mongoose
+.connect(process.env.MONGODB_URI)
+.then(() => console.log("BD Connected successfully."))
+.catch((err) => console.log("Failed to connect to DB", err))
 app.use(express.json())
 
 const PORT = process.env.PORT || 5000
@@ -14,6 +28,19 @@ const PORT = process.env.PORT || 5000
 app.get("/", async(req, res)=> {
     res.status(200).json({
         message:"Welcome to Project Management API"
+    })
+})
+
+// error middleware
+app.use((err, req, res, next) => {
+    console.log(err.stack)
+    res.status(500).json({message: "Internal Server Error"})
+})
+
+// not found
+app.use((req, res) => {
+    res.status(404).json({
+        message: "Not found"
     })
 })
 
